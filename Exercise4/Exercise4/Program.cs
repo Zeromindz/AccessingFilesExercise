5adee5df3dfc05802eb99b2372a8095862633465
James K <james_kemeny@hotmail.com>
2021-02-23 09:57:52 +1100

    Added text file writing capabilities

---
+++ Exercise4/Exercise4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exercise4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Open the file
+			using (StreamWriter writer = new StreamWriter("Test.txt"))
+			{
+				// Do writing here
+				writer.WriteLine("Hello World!");
+				//Close the file
+				writer.Close();
+			}   // Disposal happens here
+
+			Console.ReadLine();
+		}
+	}
+}
